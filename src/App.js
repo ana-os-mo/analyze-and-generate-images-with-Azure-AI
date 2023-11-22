1ea173ef3bdb07410bdb64dc858c065d2cdd9bd7
Ana Osorio <anmosoriomo@unal.edu.co>
2023-11-21 22:06:03 -0500

    base app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './App.css';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [input, setInput] = useState('');
+
+  return (
+    <div>
+      <h1>Image Analyzer and Generator</h1>
+      <input
+        type="text"
+        value={input}
+        onChange={e => setInput(e.target.value)}
+        placeholder="Enter image URL or describe an image"
+      />
+      <button className="analyze-button">Analyze Image</button>
+      <button className="create-button">Generate Image</button>
+    </div>
+  );
 }
 
 export default App;
